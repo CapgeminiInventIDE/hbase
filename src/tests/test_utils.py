5bcf51fb7163830524dc9265d8446576b8881945
ghandic <andy.challis@capgemini.com>
2021-08-20 11:16:56 +1000

    Added utils tests

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,1 +1,13 @@
+import pytest
 
+from ..hbase.utils import to_base64, from_base64
+
+
+@pytest.mark.parametrize("s,e", [("aGVsbG8=", "hello"), ("MTIzNDU=", "12345"), ("", ""),])
+def test_from_base64(s, e):
+    assert from_base64(s) == e
+
+
+@pytest.mark.parametrize("s,e", [("hello", "aGVsbG8="), ("12345", "MTIzNDU="), ("", ""),])
+def test_to_base64(s, e):
+    assert to_base64(s) == e
